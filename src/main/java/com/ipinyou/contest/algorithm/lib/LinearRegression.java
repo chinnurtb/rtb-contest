6e79572f678536e9391f6d4ed488d518b0e4f9e9
yguan <pipehappy@gmail.com>
2013-11-04 11:31:53 -0500

    update dependence

---
+++ src/main/java/com/ipinyou/contest/algorithm/lib/LinearRegression.java
@@ -0,0 +1,9 @@
+package com.ipinyou.contest.algorithm.lib;
+
+/**
+ * User: yguan
+ * Date: 11/3/13
+ * Time: 10:43 AM
+ */
+public class LinearRegression {
+}
---
+++ src/test/java/com/ipinyou/contest/algorithm/lib/LinearRegressionTest.java
@@ -0,0 +1,21 @@
+package com.ipinyou.contest.algorithm.lib;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * User: yguan
+ * Date: 11/3/13
+ * Time: 11:46 AM
+ */
+public class LinearRegressionTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+}
